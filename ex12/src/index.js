831e3868725e75b2410dc83f8f3bd33005deb892
Lunjalook <azra.cosickic@gmail.com>
2022-04-08 07:39:19 +0200

    Adding exercise 12 - Splitting Array

--- ex12/src/index.js
+++ ex12/src/index.js
@@ -1,13 +1,11 @@
 function splitArrayInGroups(arr, n) {
     var result = [];
-    var i = 0;
-
-    while (i < arr.length) {
-        result.push(arr.slice(i, i += n))
+    for (var i = 0; i < arr.length; i += n) {
+        result.push(arr.slice(i, i + n));
     }
-
     return result;
 }
+
 
 console.log(splitArrayInGroups(["a", "b", "c", "d"], 2));
 console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
