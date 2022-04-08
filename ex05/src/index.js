92505e9b48d965b6499de6bac2fa365f41a10578
Lunjalook <Azra@admins-MacBook-Air.local>
2022-04-08 23:20:43 +0200

    Adding exercise 05-Store Multiple Values in one variable

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,13 +1,13 @@
 var monitorsListArray = ["Apple", "Peach", "Berry"];
 
 function myMonitorsFunction(list) {
-    var monitorslist = [];
+    var monitorsList = [];
 
     for (var i = 0; i < list.length; i++) {
 
-        monitorslist.push([list[i], i + 1]);
+        monitorsList.push([list[i], i + 1]);
     }
-    return monitorslist;
+    return monitorsList;
 }
 console.log(myMonitorsFunction(monitorsListArray));
 
